1fd51550d8bf42527f1f7c02ac2541c5f7baeef1
munhra <rafael.munhoz@gmail.com>
2015-02-13 16:01:03 -0200

    Code updated to page 85

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,21 @@
 class UsersController < ApplicationController
 
-	def show
-		
+	def show				
+		@user = User.find(params[:id])
+	end
+
+	def edit
+		@user = User.find(params[:id])
+	end
+
+	def update
+		@user = User.find(params[:id])
+		if @user.update(user_params)
+			redirect_to @user,
+			notice: 'Cadastro atualizado com sucesso!'
+		else
+			render action: :edit
+		end
 	end
 
 	def new
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,14 +2,14 @@ class User < ActiveRecord::Base
 	#validates é uma class macro
 
 	EMAIL_REGEXP = /\A[^@]+@([\.]+\.)+[^@\.]+\z/
-	#validates_presence_of :email, :full_name, :location, :password
+	validates_presence_of :email, :full_name, :location, :password
 	validates_confirmation_of :password
-	#validates_length_of :bio, minimum: 30, allow_blank: false
-	#validates_uniqueness_of :email
+	validates_length_of :bio, minimum: 5, allow_blank: false
+	validates_uniqueness_of :email
 
 	#validacao por expressao regular
-	#def email_format
-	#	errors.add(:email, :invalid) unless email.match(EMAIL_REGEXP)
-	#end
+	def email_format
+		errors.add(:email, :invalid) unless email.match(EMAIL_REGEXP)
+	end
 
 end
